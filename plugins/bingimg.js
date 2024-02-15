b55e0e88f9da0126bb8f4adad30db9ee932f12ef
PSIKA BH <86559709+ytassad@users.noreply.github.com>
2024-02-15 21:23:08 +0100

    Create bingimg.js

---
+++ plugins/bingimg.js
@@ -0,0 +1,33 @@
+import fetch from 'node-fetch' 
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) return m.reply('هذا الامر خاص بتوليد صور من bing سوف أعطيك مثالا تكتب هكذا :\n\n*.bimg* a 26 years old boy salutes the national flag of morocco and he is wearing a shirt print is likemorocco flag and "bobiza" and 10 number anddima meghribe in small is written on it s back inbig and bold fonts, 3d illustration')
+    m.reply("تابع صاحب البوت في حساباته تشجيعا له ...\ninstagram.com/i_arther_ven")
+
+    try {
+const apikey = ["zquPZdIbFp"];
+const randomIndex = Math.floor(Math.random() * apikey.length);
+const selectedKey = apikey[randomIndex];
+        let response = await fetch(`https://api.maelyn.my.id/api/bingimage?prompt=${text}&apikey=` + apikey.getRandom())
+        let result = await response.json()
+
+        if (result.status === "Success" && result.result.length > 0) {
+            for (let i = 0; i < result.result.length; i++) {
+                setTimeout(() => {
+                    conn.sendFile(m.chat, result.result[i], `apa${i + 1}`, `instagram.com/i_arther_ven`, m)
+                }, i * 5000)
+            }
+        } else {
+            m.reply("لا يمكن العثور على الصورة التي طلبتها")
+        }
+    } catch (e) {
+        console.error(e)
+        m.reply("حدث خطأ أثناء معالجة الطلب.")
+    }
+}
+
+handler.help = ["bimg"]
+handler.tags = ["drawing"]
+handler.command = ["bimg"]
+
+export default handler
