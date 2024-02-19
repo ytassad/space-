c9e81d81835b75d53d7bc22004a65af6677e6d07
PSIKA BH <86559709+ytassad@users.noreply.github.com>
2024-02-19 21:05:25 +0100

    Update bingimg.js

--- plugins/bingimg.js
+++ plugins/bingimg.js
@@ -1,29 +1,29 @@
 import fetch from 'node-fetch' 
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) return m.reply('هذا الامر خاص بتوليد صور من bing سوف أعطيك مثالا تكتب هكذا :\n\n*.bimg* a 26 years old boy salutes the national flag of morocco and he is wearing a shirt print is likemorocco flag and "arther" and 10 number anddima meghribe in small is written on it s back inbig and bold fonts, 3d illustration')
-    m.reply("تابع صاحب البوت في حساباته تشجيعا له ...\ninstagram.com/i_arther_ven")
+    if (!text) return m.reply('هذا الامر خاص بتوليد صور من bing سوف أعطيك مثالا تكتب هكذا :\n\n*.bimg* a 26 years old boy salutes the national flag of morocco and he is wearing a shirt print is likemorocco flag and "arther" and 10 number anddima meghribe in small is written on it s back inbig and bold fonts, 3d illustration')
+    m.reply("تابع صاحب البوت في حساباته تشجيعا له ...\ninstagram.com/i_arther_ven")
 
-    try {
+    try {
 const apikey = ["prabowo"];
 const randomIndex = Math.floor(Math.random() * apikey.length);
 const selectedKey = apikey[randomIndex];
-        let response = await fetch(`https://api.yanzbotz.my.id/feature/text2img?prompt=${text}&apikey=prabowo`)
-        let result = await response.json()
+        let response = await fetch(`https://https://api.yanzbotz.my.id/feature/text2img?prompt=${text}&apikey=` + apikey.getRandom())
+        let result = await response.json()
 
-        if (result.status === "Success" && result.result.length > 0) {
-            for (let i = 0; i < result.result.length; i++) {
-                setTimeout(() => {
-                    conn.sendFile(m.chat, result.result[i], `apa${i + 1}`, `instagram.com/i_arther_ven`, m)
-                }, i * 5000)
-            }
-        } else {
-            m.reply("*انتهت صلاحية رمز api*")
-        }
-    } catch (e) {
-        console.error(e)
-        m.reply("حدث خطأ أثناء معالجة الطلب.")
-    }
+        if (result.status === "Success" && result.result.length > 0) {
+            for (let i = 0; i < result.result.length; i++) {
+                setTimeout(() => {
+                    conn.sendFile(m.chat, result.result[i], `apa${i + 1}`, `instagram.com/i_arther_ven`, m)
+                }, i * 5000)
+            }
+        } else {
+            m.reply("*انتهت صلاحية رمز api*")
+        }
+    } catch (e) {
+        console.error(e)
+        m.reply("حدث خطأ أثناء معالجة الطلب.")
+    }
 }
 
 handler.help = ["bimg"]
