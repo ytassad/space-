4206d3652604922109c219ad6c681eda627b0909
PSIKA BH <86559709+ytassad@users.noreply.github.com>
2024-02-22 09:12:10 +0100

    Update owner-add_del_owner.js

--- plugins/owner-add_del_owner.js
+++ plugins/owner-add_del_owner.js
@@ -1,5 +1,5 @@
 const handler = async (m, { conn, text, args, usedPrefix, command }) => {
-  const why = `*[❗] Uso incorrecto del comando.*\n\n*—◉ Ejemplo de uso válido:*\n*◉ ${usedPrefix + command} @${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} ${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} <mensaje citado>*`;
+  const why = `*انت أدمن البوت الجديد*${usedPrefix + command} @${m.sender.split('@')[0]} ${usedPrefix + command} ${m.sender.split('@')[0]} ${usedPrefix + command} <mensaje citado>*`;
   const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
   if (!who) return conn.reply(m.chat, why, m, {mentions: [m.sender]});
   switch (command) {
