27853a788e9e13a3cbf92d87675c45e04dc5b395
PSIKA BH <86559709+ytassad@users.noreply.github.com>
2024-02-25 10:43:15 +0100

    Update sticker-slap.js

--- plugins/sticker-slap.js
+++ plugins/sticker-slap.js
@@ -12,7 +12,7 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
   try {
     let name;
     if (who === m.chat) {
-      name = "𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝";
+      name = "kitty - md";
     } else {
       name = conn.getName(who);
     }
