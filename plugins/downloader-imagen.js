33294b5c87db4135ec7505f533bd223c68ee9531
PSIKA BH <86559709+ytassad@users.noreply.github.com>
2024-03-04 15:24:04 +0100

    Update downloader-imagen.js

--- plugins/downloader-imagen.js
+++ plugins/downloader-imagen.js
@@ -1,6 +1,6 @@
 import {googleImage} from '@bochilteam/scraper';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command} Minecraft*`;
+  if (!text) throw `*قم بالبحث عن صور مثال: ${usedPrefix + command} Minecraft*`;
   //if (m.text.includes('gore') || m.text.includes('cp')|| m.text.includes('porno')|| m.text.includes('Gore')|| m.text.includes('rule')|| m.text.includes('CP')|| m.text.includes('Rule34')) return m.reply('انتظر قليلا حتى اعثر على الصورة لاتنسى دعوة اصدقائك للمجموعة');
   const res = await googleImage(text);
   const image = await res.getRandom();
